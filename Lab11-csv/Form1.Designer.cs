cc11d3bfd7e9422a945d1f1c250fc03eb5b823a4
Adam-Rem <80329664+Adam-Rem@users.noreply.github.com>
2023-01-08 19:47:59 +0100

    Add files via upload

---
+++ Lab11-csv/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Lab11_csv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.checkBox1.Location = new System.Drawing.Point(597, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 22);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Miasto";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.checkBox2.Location = new System.Drawing.Point(597, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(122, 22);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Województwo";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.checkBox3.Location = new System.Drawing.Point(597, 68);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(93, 22);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Kod ICAO";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.checkBox4.Location = new System.Drawing.Point(745, 68);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(110, 22);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "2018 v 2019 ";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.checkBox5.Location = new System.Drawing.Point(745, 40);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(147, 22);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Liczba pasażerów";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.checkBox6.Location = new System.Drawing.Point(745, 12);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(88, 22);
+            this.checkBox6.TabIndex = 3;
+            this.checkBox6.Text = "Kod IATA";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button1.Location = new System.Drawing.Point(580, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(313, 109);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Szczegóły";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(562, 434);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 477);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Lab11-csv/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+using System.Data.Common;
+
+namespace Lab11_csv
+{
+    public partial class Form1 : Form
+    {
+
+        public DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                dt = GetDataTableFromCsv(openFileDialog.FileName, false);
+            }
+            DataSet ds = new DataSet();
+            ds.Tables.Add(dt);
+            dataGridView1.DataSource = ds.Tables[0];
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.Columns[3].Visible = false;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[5].Visible = false;
+            dataGridView1.Columns[6].Visible = false;
+        }
+
+
+
+
+        static DataTable GetDataTableFromCsv(string path, bool isFirstRowHeader)
+        {
+            string header = isFirstRowHeader ? "Yes" : "No";
+            
+
+            string pathOnly = Path.GetDirectoryName(path);
+            string fileName = Path.GetFileName(path);
+
+            string sql = @"SELECT * FROM [" + fileName + "]";
+
+
+            using (OleDbConnection connection = new OleDbConnection(
+                @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathOnly +
+                ";Extended Properties=\"Text;HDR=" + header + "\""))
+            using (OleDbCommand command = new OleDbCommand(sql,connection))
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(command))
+            {
+                DataTable dataTable = new DataTable();
+                dataTable.Locale = CultureInfo.CurrentCulture;
+                adapter.Fill(dataTable);
+                return dataTable;
+            }                
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int index = dataGridView1.CurrentCell.RowIndex;
+            string info = "";
+            //string info = dt.Rows[index][4].ToString();
+
+            if (checkBox1.Checked)
+            {
+                info = info + "\n" + dt.Rows[index][0];
+            }
+            if (checkBox2.Checked)
+            {
+                info = info + "\n" + dt.Rows[index][1];
+            }
+            if (checkBox3.Checked)
+            {
+                info = info + "\n" + dt.Rows[index][2];
+            }
+            if (checkBox4.Checked)
+            {
+                info = info + "\n" + dt.Rows[index][3];
+            }
+            if (checkBox5.Checked)
+            {
+                info = info + "\n" + dt.Rows[index][5];
+            }
+            if (checkBox6.Checked)
+            {
+                info = info + "\n" + dt.Rows[index][6];
+            }
+            Form2 fr2 = new Form2(info);
+            fr2.ShowDialog();
+
+        }
+    }
+}
